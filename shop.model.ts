d1eddda610f9a407fe3055fd2d95c9424d48c509
bypixelpro <info@pixelpro.es>
2020-03-20 16:12:42 +0100

    modelo

---
+++ shop.model.ts
@@ -0,0 +1,27 @@
+export class Shop {
+ 
+ shopItems: object;
+ 
+constructor() {
+ this.shopItems = [
+   {
+   title: 'Curso Wordpress',
+   desc: 'Aprende Wordpress desde 0 con David Piqué',
+   picture: 'wordpress.jpg',
+   price: 164
+   },
+   {
+   title: 'Curso Frontend',
+   desc: 'Aprende desarrollo front end desde 0 con David Piqué',
+   picture: 'frontend.jpg',
+   price: 220
+   },
+   {
+   title: 'Curso Fullstack',
+   desc: 'Aprende desarrollo frontend y backend desde 0 con David Piqué',
+   picture: 'fullstack.jpg',
+   price: 420
+   }
+ ];
+}
+}
